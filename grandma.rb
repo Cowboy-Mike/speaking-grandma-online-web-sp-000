7ace67c2f5428222a5406cf0b1149d48eecc40c6
Mike Nanto <mike@thecowboymechanic.com>
2021-01-02 22:11:28 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,7 +6,7 @@ def speak_to_grandma(phrase)
     phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   elseif
-    phrase == "I LOVE YOU GRANDMA!"
+    phrase == I LOVE YOU GRANDMA!
     return "I LOVE YOU TOO PUMPKIN!"
   end
 end
