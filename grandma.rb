04aadce47a1e8209a61c8131c7ca85264c81958f
Mike Nanto <mike@thecowboymechanic.com>
2021-01-02 22:18:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,11 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
-    return "HUH?! SPEAK UP, SONNY!"
+  return "HUH?! SPEAK UP, SONNY!"
   elseif phrase == phrase.upcase
-    return "NO, NOT SINCE 1938!"
+  return "NO, NOT SINCE 1938!"
   elseif phrase == "I LOVE YOU GRANDMA! "
-    return "I LOVE YOU TOO PUMPKIN!"
+  return "I LOVE YOU TOO PUMPKIN!"
   end
 end
 
